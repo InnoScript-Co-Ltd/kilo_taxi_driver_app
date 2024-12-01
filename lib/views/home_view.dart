668f2c20e714f103d31b341145efe4c114c62835
Shine Kyaw Kyaw Aung <shinekyawkyawaung@gmail.com>
2024-12-01 13:44:15 +0630

    wip: dashboard ui

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:kilo_driver_app/theme/resource/colors.dart';
+import 'package:kilo_driver_app/theme/resource/dimens.dart';
+import 'package:kilo_driver_app/views/profile_menu_view.dart';
+import 'package:latlong2/latlong.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -7,9 +12,161 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        leadingWidth: double.infinity,
+        toolbarHeight: 72.0,
+        leading: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Dimens.MARGIN_LARGE),
+          child: Row(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const ProfileMenuView()),
+                  );
+                },
+                child: Container(
+                  width: 48.0,
+                  height: 48.0,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: INDIGO_COLOR, // Stroke color
+                      width: 3, // Stroke width
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: Image.asset('assets/images/user_avatar.png'),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      const Text(
+                        "KTS - 0001",
+                        style: TextStyle(
+                            fontSize: 16.0, fontWeight: FontWeight.w600),
+                      ),
+                      const SizedBox(
+                        width: 8.0,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 4.0, horizontal: 8.0),
+                        decoration: BoxDecoration(
+                            color: PRIMARY_COLOR,
+                            borderRadius: BorderRadius.circular(16.0)),
+                        child: const Text(
+                          'kilo +',
+                          style: TextStyle(
+                              fontSize: 12, fontWeight: FontWeight.w600),
+                        ),
+                      )
+                    ],
+                  ),
+                  const Row(
+                    children: [
+                      Text(
+                        "Wallet Balance : ",
+                        style: TextStyle(
+                            fontSize: 12.0, fontWeight: FontWeight.w400),
+                      ),
+                      Text("100,000 MMK",
+                          style: TextStyle(
+                              fontSize: 12.0, fontWeight: FontWeight.w600))
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons
+                .notifications_none), // Trailing icon (e.g., settings icon)
+            onPressed: () {
+              // Action for the trailing icon
+            },
+          ),
+        ],
       ),
-      body: const Center(child: Text("Home")),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            FlutterMap(
+                options: const MapOptions(
+                  initialCenter: LatLng(16.8395368, 95.8518913),
+                  initialZoom: 12.0,
+                ),
+                children: [
+                  TileLayer(
+                      urlTemplate:
+                          "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                      userAgentPackageName: 'com.example.app',
+                      subdomains: const ['a', 'b', 'c']),
+                  MarkerLayer(markers: [
+                    Marker(
+                      point: const LatLng(16.8395368, 95.8518913),
+                      width: 40,
+                      height: 40,
+                      child: Image.asset('assets/images/car_marker.png'),
+                    )
+                  ]),
+                ]),
+            Positioned(
+                bottom: 24.0,
+                right: 24.0,
+                left: 24.0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    MaterialButton(
+                      onPressed: () {},
+                      color: const Color(0xFF0E9347),
+                      shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(8), // Rounded corners
+                      ),
+                      elevation: 0,
+                      child: const Text(
+                        'Auto On',
+                        style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white),
+                      ),
+                    ),
+                    MaterialButton(
+                      onPressed: () {},
+                      color: const Color(0xFF0909C4),
+                      shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(8), // Rounded corners
+                      ),
+                      elevation: 0,
+                      child: const Text(
+                        'Instant Ride',
+                        style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ))
+          ],
+        ),
+      ),
     );
   }
 }
